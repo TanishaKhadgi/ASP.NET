54e75ac7c4c05066f6564b454c6702248468c16b
DipakKumar6-code <126050058+DipakKumar6-code@users.noreply.github.com>
2024-10-10 12:19:42 -0400

    Successfully created programmingtemplate model

--- ProgrammingTemplateAppSol/ProgrammingTemplateApp/Models/ProgrammingTemplate.cs
+++ ProgrammingTemplateAppSol/ProgrammingTemplateApp/Models/ProgrammingTemplate.cs
@@ -4,13 +4,16 @@ namespace ProgrammingTemplateApp.Models
 {
     public class ProgrammingTemplate
     {
-        public int ProgrammingTemplateId { get; set; }
-        public string? Title { get; set; }
-        public string? Description { get; set; }
+        public int ProgrammingTemplateId { get; set; } // Primary key which will be automatically assigned
+        [Required(ErrorMessage = "Title is required"), MaxLength(80)]
+        public string Title { get; set; } // Title of the programming template
+        [Required(ErrorMessage = "Description is required"), MaxLength(250)]
+        public string Description { get; set; } // Description of the template created by users
         [DisplayFormat(DataFormatString = "{0:c}")]
-        public decimal? Price { get; set; }
-        public string? ProgrammingLanguage { get; set; }
-        public DateTime? CreatedDate { get; set; }
+        public decimal? Price { get; set; } // Price of the template at users want to sell it
+        [Required(ErrorMessage = "Programming language is required"), MaxLength(60)]
+        public string ProgrammingLanguage { get; set; } // Programming language in which the template is created
+        public DateTime? CreatedDate { get; set; } // Date and time in which the template is created
         public ICollection<Purchase>? Purchases { get; set; }
     }
 }
